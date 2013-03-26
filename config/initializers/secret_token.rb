62431e07c93aed43116dcd64e3711803c54080f5
Nícolas Iensen <nicolas@engage.is>
2013-03-26 17:18:20 -0300

    Rails template + foundations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remotando::Application.config.secret_token = '35d584950baa34238c91ea6e344e5d9175a40e2defd3847bc5b2ad30225f4ea46417175b490cb4d4cf49c33d819a0a68f4fb0994f33f023314c875d38b9ce474'
